5089a33eaa62cdc2b22254910585a70edf67b942
DanielGaraboaPaz <angeldaniel.garaboa@usc.es>
2021-01-04 13:11:53 +0100

    added IR_UK folder templates- Delete Spain deprecated

---
+++ IR_UK_Rivers/RunCase.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+clear
+
+# Activate conda environment from shell. Minimum conda version required 4.6
+eval "$(conda shell.bash hook)"
+conda activate MOHID-Lagrangian
+
+# increase stacksize
+ulimit -s hard
+export KMP_STACKSIZE=1G
+
+# Read the MOHIDLagrangianPath
+source ../MOHIDLagrangianPath.sh
+
+# "name" and "dirout" are named according to the testcase
+name=${PWD##*/}_case
+
+if [ -z ${dirout+x} ]; 
+then 
+    dirout=${name}_out
+else 
+    dirout=${dirout}${name}_out
+fi
+
+# "executables" are renamed and called from their directory
+tools=${MOHIDLagrangianPath}/build/bin
+mohidlagrangian=${tools}/MOHIDLagrangian
+
+preprocessorDir=${MOHIDLagrangianPath}/src/MOHIDLagrangianPreProcessor
+mohidPreprocessor=${preprocessorDir}/MOHIDLagrangianPreProcessor.py
+
+postProcessorDir=${MOHIDLagrangianPath}/src/MOHIDLagrangianPostProcessor
+mohidPostprocessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
+
+# "dirout" is created to store results or it is cleaned if it already exists
+if [ -e $dirout ]; then
+  rm -f -r $dirout
+fi
+mkdir -p $dirout
+
+cp ${name}.xml $dirout/
+
+# CODES are executed according the selected parameters of execution in this testcase
+
+python $mohidPreprocessor -i $dirout/${name}.xml -o $dirout
+
+errcode=0
+if [ $errcode -eq 0 ]; then
+  $mohidlagrangian -i $dirout/${name}.xml -o $dirout
+  errcode=$?
+fi
+
+python -W ignore $mohidPostprocessor -i ${name}.xml -o $dirout
+
+if [ $errcode -eq 0 ]; then
+  echo All done
+else
+  echo Execution aborted
+fi
+read -n1 -r -p "Press any key to continue..." key
+echo
---
+++ IR_UK_Rivers/RunPostProcessor.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+
+# Activate conda environment from shell. Minimum conda version required 4.6
+eval "$(conda shell.bash hook)"
+conda activate MOHID-Lagrangian
+
+# increase stacksize
+ulimit -s hard
+export KMP_STACKSIZE=1G
+
+# Read the MOHIDLagrangianPath
+source ../MOHIDLagrangianPath.sh
+
+# "name" and "dirout" are named according to the testcase
+name=${PWD##*/}_case
+
+if [ -z ${dirout+x} ]; 
+then 
+    dirout=${name}_out
+else 
+    dirout=${dirout}${name}_out
+fi
+
+# "executables" are renamed and called from their directory
+
+postProcessorDir=${MOHIDLagrangianPath}/src/MOHIDLagrangianPostProcessor
+postProcessor=${postProcessorDir}/MOHIDLagrangianPostProcessor.py
+
+# CODES are executed according the selected parameters of execution in this testcase
+errcode=0
+
+python -W ignore $postProcessor -i ${name}.xml -o $dirout
+
+if [ $errcode -eq 0 ]; then
+  echo All done
+else
+  echo Execution aborted
+fi
+read -n1 -r -p "Press any key to continue..." key
+echo
